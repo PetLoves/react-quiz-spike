5126810d95b7dcfc4b5347562c69bf5cdf3791df
Jyoti <jyoti.contact@gmail.com>
2020-03-10 20:19:57 +0000

    quiz code

---
+++ src/api/quizQuestions.js
@@ -0,0 +1,90 @@
+var quizQuestions = [
+  {
+    question: 'What kind of animal do you want to rescue? ',
+    answers: [
+      {
+        type: 'Stark',
+        content: 'Dog'
+      },
+
+      {
+        type: 'Lannister',
+        content: 'Cat'
+      },
+      {
+        type: 'Targaryen',
+        content: 'Unicorn'
+      }
+    ]
+  },
+  {
+    question: 'What do you value most ? ',
+    answers: [
+      {
+        type: 'Stark',
+        content: 'Loyalty'
+      },
+      {
+        type: 'Lannister',
+        content: 'Money'
+      },
+      {
+        type: 'Targaryen',
+        content: 'Power'
+      }
+    ]
+  },
+  {
+    question: "Who's your favorite family member ?",
+    answers: [
+      {
+        type: 'Stark',
+        content: 'My Parent'
+      },
+      {
+        type: 'Lannister',
+        content: 'My Sibling'
+      },
+      {
+        type: 'Targaryen',
+        content: 'My Grandparent'
+      }
+    ]
+  },
+  {
+    question: 'Choose an element',
+    answers: [
+      {
+        type: 'Stark',
+        content: 'Water'
+      },
+      {
+        type: 'Lannister',
+        content: 'Air'
+      },
+      {
+        type: 'Targaryen',
+        content: 'Fire'
+      }
+    ]
+  },
+  {
+    question: ' Which Castle would you live in ? ',
+    answers: [
+      {
+        type: 'Stark',
+        content: 'Winterfell'
+      },
+      {
+        type: 'Lannister',
+        content: 'Casterly Rock'
+      },
+      {
+        type: 'Targaryen',
+        content: 'DragonStone'
+      }
+    ]
+  }
+];
+
+export default quizQuestions;
